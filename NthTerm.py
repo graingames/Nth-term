2617f154c41126200d6ad52159fe4d55769e73a3
graingames <98623348+graingames@users.noreply.github.com>
2022-10-24 16:55:21 +0100

    Added project

---
+++ NthTerm.py
@@ -0,0 +1,8 @@
+#Made 26/07/2022
+print("\n\n\n\n")
+print("Enter the first 2 terms ")
+term1=float(input("Term1: "))
+term2=float(input("Term2: "))
+coefficient=term2-term1
+additive=term2-coefficient
+input(str(coefficient)+"n + "+str(additive))
